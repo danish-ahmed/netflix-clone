fa84faf19e7c17cb9a2b3ab36ebc197839bdc536
Danish_Ahmed <qureshidanish78@gmail.com>
2020-12-21 20:51:30 +0500

    home page complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
-import { JumbotronContainer } from "./containers/jumbotron";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Signup from "./pages/signup";
+import Signin from "./pages/signin";
 function App() {
-  return <JumbotronContainer />;
+  return (
+    <Router>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
+      <Route path="/signin">
+        <Signin />
+      </Route>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,32 @@
+import { JumbotronContainer } from "../containers/jumbotron";
+import { FooterContainer } from "../containers/footer";
+import { FaqsContainer } from "../containers/faqs";
+import { HeaderContainer } from "../containers/header";
+import { OptForm } from "../components";
+import { Feature } from "../components";
+export default function home() {
+  return (
+    <div>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>
+            Unlimited films, TV Programmes and more.
+          </Feature.Title>
+          <Feature.SubTitle>Watch anywhere. Cancel any time.</Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </div>
+  );
+}
